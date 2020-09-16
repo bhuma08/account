c94e263a0fa7b2d9aa3847b757f2217f95365c82
bhuma08 <62709043+bhuma08@users.noreply.github.com>
2020-09-16 21:05:04 +0100

    sign up page created!

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import LoginCard from './containers.js/login'
 import AddNewPage from './pages.js/AddNewPage'
 import DashboardPage from './pages.js/DashboardPage'
 import AllPosts from './containers.js/allposts'
+import SignUpCard from './containers.js/signup'
 
 export class App extends Component {
     render() {
@@ -14,6 +15,7 @@ export class App extends Component {
                     <Route path='/:id/dashboard' component={ DashboardPage }/>
                     <Route path='/:id/addnew' component={ AddNewPage }/>
                     <Route path='/login' component={ LoginCard }/>
+                    <Route path='/signup' component={ SignUpCard }/>
                     <Route path='/posts' component={ AllPosts }/>
                 </Switch>
               
---
+++ client/src/containers.js/signup.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
+
+class SignUpCard extends Component {
+
+
+     state = {
+            name: '',
+            email: '',
+            password: ''
+
+    }
+
+    handleChange(e) {
+        this.setState({ name: e.target.name.value})
+        this.setState({ email: e.target.email.value })
+        this.setState({ email: e.target.password.value })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        
+        const data = {
+            email: e.target.email.value,
+            password: e.target.password.value
+        }
+
+        console.log(data)
+    
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {"Content-Type": "application/json"}
+        }
+    
+        fetch('http://localhost:3000/user', options)
+            .then(r => r.json())
+            .then(this.props.history.push(`/login`))
+            .catch((console.warn()))     
+
+    }
+
+    render() {
+        return (
+            <div>
+               
+                <form onSubmit={this.handleSubmit} className="login-form">
+                    <label>Name</label>
+                    <input type="text" name="name" id="name" onChange={this.handleChange}></input>
+                    <label>Email</label>
+                    <input type="text" name="email" id="email" onChange={this.handleChange}></input>
+                    <label>Password</label>
+                    <input type="password" name="password" onChange={this.handleChange} id="password"></input>
+                    <input type='submit'></input>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default withRouter(SignUpCard);
